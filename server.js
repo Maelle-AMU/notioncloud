991bffedb092939b02d70d20df42af129cfa6e45
Maelle-AMU <maelle.goujat@etu.univ-amu.fr>
2025-11-28 09:19:10 +0100

    test avec 3010

--- server.js
+++ server.js
@@ -94,6 +94,6 @@ app.get('/api/classement', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3010;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
