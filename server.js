28bf74560250c3063b370ed3f890b6a1135bc76a
Maelle-AMU <maelle.goujat@etu.univ-amu.fr>
2025-12-03 09:44:55 +0100

    update

--- server.js
+++ server.js
@@ -106,32 +106,7 @@ app.get('/api/matches', (req, res) => {
 
 
 
-// Classement route
-app.get('/api/classement', async (req, res) => {
-  try {
-    const [rows] = await pool.query("SELECT * FROM `match` WHERE status='played'");
-    const classement = {};
-    rows.forEach(m => {
-      [m.home_team, m.away_team].forEach(team => {
-        if (!classement[team]) classement[team] = { points: 0, scored: 0, conceded: 0 };
-      });
-      classement[m.home_team].scored += m.home_score;
-      classement[m.home_team].conceded += m.away_score;
-      classement[m.away_team].scored += m.away_score;
-      classement[m.away_team].conceded += m.home_score;
-      if (m.home_score > m.away_score) classement[m.home_team].points += 4;
-      else if (m.home_score < m.away_score) classement[m.away_team].points += 4;
-      else { classement[m.home_team].points += 2; classement[m.away_team].points += 2; }
-    });
-    const classementArray = Object.entries(classement)
-      .map(([team, stats]) => ({ team, ...stats }))
-      .sort((a, b) => b.points - a.points || b.scored - a.scored);
-    res.json(classementArray);
-  } catch (err) {
-    console.error('GET /api/classement error:', err.code, err.message);
-    res.status(500).json({ error: 'Erreur serveur', details: { code: err.code, message: err.message } });
-  }
-});
+
 
 
 
